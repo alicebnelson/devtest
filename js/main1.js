4d9cce06da66f5d7ea9b918f98db8effad21090c
alicebnelson <nelsonaliceb@gmail.com>
2016-11-09 22:09:42 -0500

    portfolio
    Portfolio of Alice B. Nelson

---
+++ js/main1.js
@@ -0,0 +1,102 @@
+$(document).ready(function() {
+
+console.log("Welcome to My Portfolio!");
+
+  var vid = document.getElementById("bgvid");
+  var pauseButton = document.querySelector("#logo button");
+  var $animation_elements = $(".skillbar");
+  var $window = $(window);
+  $window.on('scroll', check_if_in_view);
+  $window.on('scroll resize', check_if_in_view);
+  function vidFade() {
+    vid.classList.add("stopfade");
+  }
+
+  vid.addEventListener('ended', function()
+  {
+  // only functional if "loop" is removed
+  vid.pause();
+  // to capture IE10
+  vidFade();
+  });
+
+
+  pauseButton.addEventListener("click", function() {
+    vid.classList.toggle("stopfade");
+    if (vid.paused) {
+      vid.play();
+      pauseButton.innerHTML = "Pause";
+    } else {
+      vid.pause();
+      pauseButton.innerHTML = "Paused";
+    }
+  })
+
+
+  function check_if_in_view() {
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+          (element_top_position <= window_bottom_position)) {
+            $(this).find('.skillbar-bar').animate({
+    			width:$(this).attr('data-percent')
+    		},6000);
+      }
+    });
+  }
+
+  $(".circleBorder").on("click",function(){
+    var id=$(this).attr("id");
+    switch(id){
+      case "wireFrames":
+        $("#myModal1").modal("show");
+        break;
+      case "infoArch":
+        $("#myModal2").modal("show");
+        break;
+      case "appFlow":
+        $("#myModal3").modal("show");
+        break;
+      case "persona":
+        $("#myModal4").modal("show");
+        break;
+      case "dishwasher":
+        $("#myModal5").modal("show");
+        break;
+      case "data":
+        $("#myModal6").modal("show");
+        break;
+      case "email1":
+        $("#myModal7").modal("show");
+        break;
+      case "email2":
+        $("#myModal8").modal("show");
+        break;
+      case "banner":
+        $("#myModal9").modal("show");
+        break;
+      case "certificateLogo":
+        $("#myModal10").modal("show");
+        break;
+      case "cattire":
+        $("#myModal11").modal("show");
+        break;
+      case "signup":
+        $("#myModal12").modal("show");
+        break;
+    }
+
+
+  });
+
+
+});
